7dd224fa9357cfe4584b3cfba7e292310382267a
David Herzberg <dherzberg@wpspublish.com>
2019-12-26 23:30:54 -0800

    deal with multiple forms

--- CODE/DP4_scratch.R
+++ CODE/DP4_scratch.R
@@ -2,31 +2,35 @@ suppressMessages(library(here))
 suppressMessages(library(tidyverse))
 suppressMessages(library(readxl))
 
-file_name <- c('Norms_RawtoSS_InterviewForm_ITTables_DH SPECS')
+file_name <- c('Norms_RawtoSS_InterviewForm_ITTables_DH SPECS',
+               'Norms_RawtoSS_ParentChecklist_ITTables_DH SPECS',
+               'Norms_RawtoSS_TeacherNorms_ITTables_DH SPECS')
 
 perc_lookup <- suppressMessages(read_csv(here('INPUT-FILES/Percentile-Lookup-SS.csv')))
 
+lookup <- function(x) {
 # express the directory path to the input file as a string.
 path <- here(
-paste0('INPUT-FILES/OES-TABLES/', file_name, '.xlsx'))
+paste0('INPUT-FILES/OES-TABLES/', x, '.xlsx'))
 
 # input file is multi-tabbed .xlsx. Tabs contain lookup tables for each
 # agestrat. read input file into a df, stacking tabs on top of one another, and
 # creating a new column 'agestrat' to identify the origin tab of each set of rows.
-temp2 <- path %>% 
+path %>% 
   excel_sheets() %>%
   set_names() %>%
   map_df(read_excel,
       path = path,
-      .id = 'agestrat')
+      .id = 'agestrat') %>% 
+  assign('input_lookup', ., envir = .GlobalEnv)
 
 # recode agestrat so that it will sort properly
-str_sub(temp2$agestrat, 4)  <- str_sub(temp2$agestrat, 4) %>% 
+str_sub(input_lookup$agestrat, 4)  <- str_sub(input_lookup$agestrat, 4) %>% 
   str_pad(3, side = 'left', '0') 
 
 # Transform input table so that subscales and their associated raw-to-SS lookups
 # are nested within each value of agestrat.
-temp3 <- temp2 %>% 
+input_lookup %>% 
   gather('scale', 'SS', -agestrat, -rawscore) %>%
   right_join(perc_lookup, by = 'SS') %>% 
   arrange(scale, agestrat) %>% 
@@ -39,4 +43,11 @@ temp3 <- temp2 %>%
     TRUE ~ NA_character_
   )) %>% 
   select(scale, agestrat, rawscore, SS, descrange, Percentile)
-  
+}
+
+lookup_list <- file_name %>% map(lookup)
+
+interview_lookup <- lookup_list[[1]]
+parent_lookup <- lookup_list[[2]]
+teacher_lookup <- lookup_list[[3]]
+
