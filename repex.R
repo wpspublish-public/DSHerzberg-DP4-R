0b21a88ca4767c17de523dae9796ea8c988e679c
David Herzberg <dherzberg@wpspublish.com>
2020-01-05 21:52:18 -0800

    apply function to list of dfs

--- repex.R
+++ repex.R
@@ -1,4 +1,4 @@
-library(tidyverse)
+suppressMessages(library(tidyverse))
 
 # create data
 table_names <- c('dfA', 'dfB', 'dfC')
@@ -8,10 +8,10 @@ dfB <- tibble(a = 10:12, b = 13:15, c = 16:18)
 dfC <- tibble(a = 19:21, b = 22:24, c = 25:27)
 df_list <- list(dfA, dfB, dfC) %>% setNames(table_names)
 
-# function
-dfA_mod <- df_list$dfA %>% 
-  mutate(name = 'dfA') %>%
-  select(name, everything()) 
+# function: this this expresses the required transformations for a single df
+# dfA_mod <- df_list$dfA %>% 
+#   mutate(name = 'dfA') %>%
+#   select(name, everything()) 
 
 # stack overflow answer: use purr::imap() to apply a function to both a list and
 # its index (in a named list the index is the names of the elements)
@@ -22,4 +22,9 @@ df_out <- imap(df_list, ~.x %>% mutate(name = .y) %>% select(name, everything())
 names(df_out) <- paste0(names(df_out), "_mod")
 list2env(df_out, .GlobalEnv)
 
+# more options from SO, both stack all three dfs with a new `name` column in the
+# far left position
+map_dfr(df_list, I, .id = 'name')
+
+bind_rows(df_list, .id = 'name')
 
