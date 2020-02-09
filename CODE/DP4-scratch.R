5bf39ef0dc9a57f20bfa866f32df8e94ea175989
David Herzberg <dherzberg@wpspublish.com>
2020-02-08 16:30:27 -0800

    conceptualize CV input as mutli- file/tab/column

--- CODE/DP4-scratch.R
+++ CODE/DP4-scratch.R
@@ -4,8 +4,7 @@ suppressMessages(library(readxl))
 
 form <- c('parent', 'teacher', 'self')
 scale_acr <- c('COG', 'EMO')
-CV <- c(90, 95)
-
+CV <- c('90', '95')
 
 scale_readin <- function(x) {
   # express the directory paths to the input files as a char vec.
@@ -32,13 +31,29 @@ scale_lookup_pre <- form %>%
   bind_rows(.id = 'form')
 
 # Read in CV .xlsx
-map_df(CV, ~ here(paste0('DOCUMENTATION/DEMO-INPUT-TABLES/CV', .x, '.xlsx')) %>% 
-  excel_sheets() %>%
-  set_names() %>%
-  map_df(read_excel,
-         path = here(paste0('DOCUMENTATION/DEMO-INPUT-TABLES/CV', .x, '.xlsx')),
-         .id = 'form') %>% 
-  assign(paste0('CV_', .x, '_lookup'), ., envir = .GlobalEnv)
-)
+CV_readin <- function(x) {
+  # express the directory paths to the input files as a char vec.
+  here(
+    paste0('DOCUMENTATION/DEMO-INPUT-TABLES/CV', x, '.xlsx')) %>%
+    assign('path', ., envir = .GlobalEnv)
+  
+  
+  # input file is multi-tabbed .xlsx. Tabs contain lookup tables for each
+  # agestrat. read input file into a df, stacking tabs on top of one another, and
+  # creating a new column 'agestrat' to identify the origin tab of each set of rows.
+  path %>% 
+    excel_sheets() %>%
+    set_names() %>%
+    map_df(read_excel,
+           path = path,
+           .id = 'form') %>% 
+    assign('CV_lookup', ., envir = .GlobalEnv)
+}
+
+CV_lookup_pre <- CV %>% 
+  map(CV_readin) %>% 
+  setNames(CV) %>% 
+  reduce(full_join, by = c('form', 'agestrat'))
+
 
 
